a4e12ba0da15b69ff3a146e0ed7a02ed43e345dc
AasthaRai17 <77830618+AasthaRai17@users.noreply.github.com>
2021-02-01 23:59:42 +0530

    adding fresh

--- Assignments/src/com/Planon/Bank/BankProcess.java
+++
@@ -1,9 +0,0 @@
-package com.Planon.Bank;
-
-public class BankProcess {
-	
-	public static void main(String[] args) {
-		ProcessTransaction bankProcess = new ProcessTransaction();
-		bankProcess.initiateTheBankTransactionProcess();
-	}
-}
